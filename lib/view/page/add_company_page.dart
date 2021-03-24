303395eeb775e4645e62454c071067616d975fc3
shokiogawa <syoki2578@outlook.jp>
2021-03-24 21:19:33 +0900

    add: firebase関連挿入

--- lib/view/page/add_company_page.dart
+++ lib/view/page/add_company_page.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 class AddCompanyPage extends StatelessWidget {
+  final TextEditingController _companyNameController = TextEditingController();
+  final TextEditingController _memoController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(icon: Text("保存"), onPressed: null)
+        ],
         title: Text("会社を入力"),
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextField(
+                decoration: InputDecoration(hintText: "会社名"),
+                maxLines: 1,
+                controller: _companyNameController,
+              ),
+            ),
+          ),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("メモ"),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextField(
+                    maxLines: 10,
+                    controller: _memoController,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
